edd609b5dbb11a0c426ce598a2e60ac151615b6b
Ozgur Ergin <80709273+kodbutonu@users.noreply.github.com>
2024-09-28 21:33:49 +0300

    Added user settings

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Fragment } from 'react';
 import Counter from './components/Counter';
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
+import Header from './components/Header';
+import { useSelector} from 'react-redux';
 
 
 function App() {
+ 
+  const isAuth=useSelector(state=>state.auth.isAuthenticated)
+
   return (
-    <Counter />
+    <Fragment>
+      <Header/>
+      {isAuth?(<UserProfile/>):(<Auth/>)}
+      <Counter />
+    </Fragment>
+
   );
 }
 
